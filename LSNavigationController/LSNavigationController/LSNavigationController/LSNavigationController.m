0619f0c2e6c0e650598bd8dc51bf4daa85d33e9e
liusong <song.liu@mytogo.com>
2018-12-17 10:34:55 +0800

    no message

--- LSNavigationController/LSNavigationController/LSNavigationController/LSNavigationController.m
+++ LSNavigationController/LSNavigationController/LSNavigationController/LSNavigationController.m
@@ -55,14 +55,13 @@
     [super layoutSubviews];
     for (UIView *view in self.subviews) {
         NSString *className=NSStringFromClass([view class]);
-        if ([className isEqualToString:@"_UIBarBackground"]) {
+        if ([className isEqualToString:@"_UIBarBackground"]||[className isEqualToString:@"_UINavigationBarBackground"]) {
             
             CGFloat height=[UIApplication sharedApplication].statusBarFrame.size.height;
             CGRect frame=self.bounds;
             frame.size.height =self.frame.size.height+height;
             frame.origin.y=-height;
             view.frame=frame;
-            
         }
     }
 }
